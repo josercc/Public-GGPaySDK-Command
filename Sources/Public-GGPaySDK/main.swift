87cfc7a61f55e7f47c3c585fa46f0d0becb845ea
张行 <zhanghang@globalegrow.com>
2019-12-06 10:59:32 +0800

    fix branch

--- Sources/Public-GGPaySDK/main.swift
+++ Sources/Public-GGPaySDK/main.swift
@@ -1,7 +1,8 @@
 import Commander
 
 let versionCommand = command { (version:String, branch:String, sourceURL:String, sdkURL:String, reposURL:String) in
-    let publicVersion = Public(version:version, branch:branch, sourceURL:sourceURL, sdkURL:sdkURL, reposURL:reposURL)
+    let list = branch.components(separatedBy: "/")
+    let publicVersion = Public(version:version, branch:list.last!, sourceURL:sourceURL, sdkURL:sdkURL, reposURL:reposURL)
     do {
         try publicVersion.publicVersion()
     } catch let error {
