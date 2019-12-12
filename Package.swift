eee4917dd5a423b33d8ad645e9654fe299646d88
张行 <zhanghang@globalegrow.com>
2019-12-12 15:19:41 +0800

    support Mint install: mint run josercc/Public-GGPaySDK-Command

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Public-GGPaySDK",
+    products: [
+        .executable(name: "pgg", targets: ["Public-GGPaySDK"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -23,3 +26,4 @@ let package = Package(
             dependencies: ["Public-GGPaySDK"]),
     ]
 )
+
